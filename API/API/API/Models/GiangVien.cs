b72f438d4a2198f3dfc1670b2ef7ad0fb7a87933
Huynhtrung1006 <huynhtrung1006@gmail.com>
2021-06-25 12:24:25 +0700

    Update file date 25062021

---
+++ API/API/API/Models/GiangVien.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace API.Models
+{
+    public partial class GiangVien
+    {
+        public string MaGv { get; set; }
+        public string TenGv { get; set; }
+        public string Email { get; set; }
+        public string Diachi { get; set; }
+        public string Sdt { get; set; }
+        public string Hocham { get; set; }
+        public DateTime Ngaysinh { get; set; }
+        public string Matkhau { get; set; }
+        public bool Trangthai { get; set; }
+        public string Hinhanh { get; set; }
+        public string MaChucVu { get; set; }
+        public string MaKhoa { get; set; }
+
+        public virtual ChucVu MaChucVuNavigation { get; set; }
+        public virtual Khoa MaKhoaNavigation { get; set; }
+        public virtual LopMonHocGiangVien LopMonHocGiangVien { get; set; }
+    }
+}
---
+++ API/API/API/Models/SinhVien.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace API.Models
+{
+    public partial class SinhVien
+    {
+        public SinhVien()
+        {
+            PhieuDangKies = new HashSet<PhieuDangKy>();
+            PhucKhaos = new HashSet<PhucKhao>();
+        }
+
+        public string MaSv { get; set; }
+        public string TenSv { get; set; }
+        public string Diachi { get; set; }
+        public DateTime Ngaysinh { get; set; }
+        public bool Phai { get; set; }
+        public string Email { get; set; }
+        public string Cnmd { get; set; }
+        public string Hinhanh { get; set; }
+        public string Matkhau { get; set; }
+        public string MaLop { get; set; }
+
+        public virtual Lop MaLopNavigation { get; set; }
+        public virtual ICollection<PhieuDangKy> PhieuDangKies { get; set; }
+        public virtual ICollection<PhucKhao> PhucKhaos { get; set; }
+    }
+}
