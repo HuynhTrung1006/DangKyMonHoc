5b5a9f1b449da4859b76d99894f9e53f5250857b
Huynhtrung1006 <huynhtrung1006@gmail.com>
2021-06-26 09:45:13 +0700

    add branch Trung_QLND

---
+++ API/API/API/Controllers/FileUploadsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadsController : ControllerBase
+    {
+        public static IWebHostEnvironment _webHostEnvironment;
+        public FileUploadsController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public string post([FromForm]FileUpload objectFile)
+        {
+            try
+            {
+                if (objectFile.files.Length>0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\uploads\\";//AppDomain.CurrentDomain.BaseDirectory + @"IMG\"; API/bin/debug/IMG/?
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    using (FileStream fileStream= System.IO.File.Create(path + objectFile.files.FileName))
+                    {
+                        objectFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "Uploaded";
+                    }
+                }
+                else
+                {
+                    return "Not Uploaded";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+                //throw;
+            }
+        }
+    }
+}
