3365056cdcdb08a3339da154c91ee0aaa4ae8a6d
Huynhtrung1006 <huynhtrung1006@gmail.com>
2021-06-28 21:33:43 +0700

    add xuly

---
+++ API/API/API/XuLy/Input_Ma.cs
@@ -0,0 +1,32 @@
+﻿using API.Models;
+using BCrypt.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.XuLy
+{
+    public class Input_Ma
+    {
+        private readonly DangKyMonHocContext db = new DangKyMonHocContext();
+        //xu ly ma sinh vien
+        public string maSinhVien(string masv, string malop)
+        {
+            var lop = db.Lops.FirstOrDefault(x => x.MaLop == malop);
+            var nganh = db.Nganhs.FirstOrDefault(x => x.MaNganh == lop.MaNganh);
+            var ctdt = db.ChuongTrinhDaoTaos.FirstOrDefault(x => x.MaNganh == nganh.MaNganh);
+
+            var nienkhoa = db.NienKhoas.FirstOrDefault(x => x.MaNk == lop.MaNk);
+            masv = ctdt.MaDt.Trim() + nganh.MaKhoa.Trim() + nienkhoa.MaNk.Trim() + masv.Trim();
+
+            return masv;
+        }
+
+        public string hashPassword(string pass)
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword(pass);
+            return hash;
+        }
+    }
+}
