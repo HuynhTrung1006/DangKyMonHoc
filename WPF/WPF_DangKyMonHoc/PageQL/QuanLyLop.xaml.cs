4ae60be1350a33ac08d2c867f32665c7ef328b50
Huynhtrung1006 <huynhtrung1006@gmail.com>
2021-07-02 00:13:14 +0700

    Add file page and window

---
+++ WPF/WPF_DangKyMonHoc/PageQL/QuanLyLop.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf_DangKyMonHoc.WindowQL;
+
+namespace Wpf_DangKyMonHoc.PageQL
+{
+    /// <summary>
+    /// Interaction logic for QuanLyLop.xaml
+    /// </summary>
+    public partial class QuanLyLop 
+    {
+        public QuanLyLop()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Nganh(object sender, RoutedEventArgs e)
+        {
+            var n = new QuanLyNganh();
+            n.ShowDialog();
+        }
+
+        private void btn_NK(object sender, RoutedEventArgs e)
+        {
+            var n = new QuanLyNienKhoa();
+            n.ShowDialog();
+        }
+    }
+}
